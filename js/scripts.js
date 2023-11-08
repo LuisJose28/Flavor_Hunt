f9a373f585dbbf25b66f5116170169e298d7da3b
Luis Espinosa <82414418+LuisJose28@users.noreply.github.com>
2023-11-08 01:14:49 -0500

    Se corrigieron problemas en el menú de navegación en la página reservation_info.html. También se implementó JavaScript y se realizaron ajustes en el código HTML para resolver problemas de responsive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/* Página RESTAURANT INFO*/
 $(document).ready(function(){
     $('.label-menu li a:first').addClass('active'); /* Aqui estamos diciendo que le agregaremos la clase active, que es el color de fondo de los titulos de los tabs al estar sobre ellos */
     $('.content article').hide(); /* Ocultaremos por defecto todos los tabs */
@@ -16,8 +17,26 @@ $(document).ready(function(){
         return false;
 
     });
-    
-    
+});
 
 
+/* Página RESERVATION INFO*/
+$(document).ready(function(){
+    $('.label-menu-reservation li a:first').addClass('active'); /* Aqui estamos diciendo que le agregaremos la clase active, que es el color de fondo de los titulos de los tabs al estar sobre ellos */
+    $('.content-reservation article').hide(); /* Ocultaremos por defecto todos los tabs */
+    $('.content-reservation article:first').show(); /* Mostraremos solo el primer tab */
+
+    $('.label-menu-reservation li a').click(function(){
+        /* Cada que demos en cualquiera de los enlaces donde demos clic, le quitaremos la clase y se lo agregaremos al que le estamos dando clic */
+        $('.label-menu-reservation li a').removeClass('active');
+        $(this).addClass('active');
+
+        $('.content-reservation article').hide(); /* Cada vez que hagamos clic en uno, todos los demas tabs se oculten */
+
+        /* Esta variable nos ayudara a mostrar solo al que le hemos dado clic */
+        var activeTab = $(this).attr('href');/* Creamos una variable y su valor sera el valor del atributo href a */
+        $(activeTab).show();
+        return false;
+
+    });
 });
